7b3405ea6e95bf7e0ec4dc9ddc69df23758d8efe
Tyler J <tylerj@21.co>
2015-08-20 08:32:44 -0700

    simplify main init

--- main.js
+++ main.js
@@ -2,17 +2,16 @@ var fs = require('fs');
 var disassemble = require('./disassembler');
 
 const CURRENT_ROM = 'PONG';   // Set current ROM for testing
-const ROM = {fd: undefined, size: undefined};
 
 (function main() {
-  /*        Open the ROM and read get its size        */
-  ROM.fd = fs.openSync(CURRENT_ROM, 'r');
-  ROM.size = fs.fstatSync(ROM.fd).size;
+  /*        Open the ROM and get its size        */
+  const romFd = fs.openSync(CURRENT_ROM, 'r');
+  const romSize = fs.fstatSync(romFd).size;
 
-  /*        Read the ROM into a buffer object as hexadecimal        */
-  const buf = new Buffer(ROM.size);
-  fs.readSync(ROM.fd, buf, 0, ROM.size, 0);
+  /*        Read the ROM into a Buffer object as hexadecimal        */
+  const romBuf = new Buffer(romSize);
+  fs.readSync(romFd, romBuf, 0, romSize, 0);
 
-  /*        Run the disassembler on the buffer        */
-  disassemble(buf);
+  /*        Run the disassembler on the romBuffer        */
+  disassemble(romBuf);
 })();
