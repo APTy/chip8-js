ff84181d8aea2dc17c040104b3c8fe45e8910782
Tyler J <tylerj@21.co>
2015-08-20 08:33:17 -0700

    add storage for program counter and stack pointer

--- memory.js
+++ memory.js
@@ -6,5 +6,7 @@ global.M  = new Uint8Array(MEMORY_BYTE_SIZE);           // Memory
 global.V  = new Uint8Array(REGISTER_BYTE_SIZE);         // Register
 global.S  = new Uint16Array(STACK_BYTE_SIZE);           // Stack
 global.I  = 0x0000;                                     // Address Register
+global.PC = 0x0000;                                     // Program Counter
+global.SP = 0x00;                                       // Stack Pointer
 global.DT = 0x0000;                                     // Delay Timer
 global.ST = 0x0000;                                     // Sound Timer
