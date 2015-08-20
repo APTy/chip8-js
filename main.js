fa135fc0949bd1f5483969a2048ba495ca4ad6bc
R. Tyler Julian <trjulian@gmail.com>
2015-08-19 22:24:24 -0700

    create disassembler to read opcodes as bytes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
 const CURRENT_ROM = 'PONG';
+const OP_CODE_BYTE_LENGTH = 2;
 const rom = {fd: undefined, size: undefined};
 
 rom.fd = fs.openSync(CURRENT_ROM, 'r');
@@ -8,8 +9,11 @@ rom.size = fs.fstatSync(rom.fd).size;
 const buf = new Buffer(rom.size);
 
 fs.readSync(rom.fd, buf, 0, rom.size, 0);
-const opcodes = buf.toString('hex');
+disassemble(buf);
 
-for (var i=0; i < buf.length; i += 4) {
-  console.log(opcodes.substring(i, i+4));
+function disassemble(buffer) {
+  for (var i=0; i < buffer.length; i += OP_CODE_BYTE_LENGTH) {
+    var x = buffer.readUIntBE(i, OP_CODE_BYTE_LENGTH);
+    console.log(x);
+  }
 }
