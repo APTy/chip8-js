4f6c6856064273183e3bcc66cf0615a6a7c18604
R. Tyler Julian <trjulian@gmail.com>
2015-08-19 22:06:47 -0700

    clean up variable instantiation

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 var fs = require('fs');
-var smalloc = require('smalloc');
 
 const CURRENT_ROM = 'PONG';
 const rom = {fd: undefined, size: undefined};
 
 rom.fd = fs.openSync(CURRENT_ROM, 'r');
 rom.size = fs.fstatSync(rom.fd).size;
+const buf = new Buffer(rom.size);
 
-var buf = new Buffer(rom.size);
-var game = fs.readSync(rom.fd, buf, 0, rom.size, 0);
-var opcodes = buf.toString('hex');
-var i;
+fs.readSync(rom.fd, buf, 0, rom.size, 0);
+const opcodes = buf.toString('hex');
 
-for (i=0; i < buf.length; i += 4) {
+for (var i=0; i < buf.length; i += 4) {
   console.log(opcodes.substring(i, i+4));
 }
