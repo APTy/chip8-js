73b48fd3b00e5cd3bf2eefd3c93dec9b18d82e33
R. Tyler Julian <trjulian@gmail.com>
2015-08-19 23:04:48 -0700

    add 4kb memory store and 16-level stack

--- opcodes.js
+++ opcodes.js
@@ -1,4 +1,11 @@
-const V = new Uint8Array(0x10);
+const MEMORY_BYTE_SIZE = 0x1000;
+const REGISTER_BYTE_SIZE = 0x10;
+const STACK_BYTE_SIZE = 0x10;
+
+const M = new Uint8Array(MEMORY_BYTE_SIZE);           // Memory
+const V = new Uint8Array(REGISTER_BYTE_SIZE);         // Register
+const S = new Uint16Array(STACK_BYTE_SIZE);           // Stack
+
 const ops = [op_0, op_1, op_2, op_3, op_4, op_5, op_6, op_7,
              op_8, op_9, op_A, op_B, op_C, op_D, op_E, op_F];
 
