89bb9ed78265013f7e94d22f516fe07f47a74521
Tyler J <tylerj@21.co>
2015-08-23 15:24:47 -0500

    added event dispatching to disassembler

--- disassembler.js
+++ disassembler.js
@@ -1,7 +1,8 @@
 require('./memory');
 require('./returnCodes');
-var debug = require('./debug');
 var ops = require('./opcodes');
+var clock = require('./clock');
+var debug = require('./debug');
 
 /* CHIP-8 has 35 opcodes, which are all two bytes long and stored Big-endian. */
 const OP_CODE_BYTE_LENGTH = 2;
@@ -11,6 +12,13 @@ function disassemble(startAddress, programLength) {
   PC = startAddress;
 
   while (PC < startAddress + programLength) {
+
+    /*        We should leave some room to handle other
+              events that the processor needs to make from
+              time to time. FIXME: add this as part of 'main'
+              or a separate 'processor' as it's not
+              heavily related to disassembly        */
+    doEvents();
 
     /*        Read in the next 2 bytes of data        */
     var instruction = (M[PC] << 8) + M[PC+1];
@@ -38,6 +46,10 @@ function disassemble(startAddress, programLength) {
       PC += OP_CODE_BYTE_LENGTH;
     }
   }
+}
+
+function doEvents() {
+  clock.tick();
 }
 
 module.exports = disassemble;
