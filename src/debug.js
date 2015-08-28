9086a8f45a19e797d1a20161844261b7128e64de
R. Tyler Julian <trjulian@gmail.com>
2015-08-27 21:04:34 -0700

    add basic readme

--- src/debug.js
+++ src/debug.js
@@ -1,8 +1,5 @@
-// var util = require('util');
-
 const DEBUG = true;
 
 exports.log = function() {
-  // if (DEBUG) util.log.apply(console, arguments);
   if (DEBUG) console.log.apply(console, arguments);
 }
