4e69656c7f413ada5b96864b76457d22186e5132
R. Tyler Julian <trjulian@gmail.com>
2016-01-23 17:23:10 -0800

    decoder: change Instruction to calculate rather than store properties

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -97,13 +97,49 @@ exports.CPU = CPU;
 **/
 function Instruction(raw) {
   this.raw = raw;
-  this.opcode = raw >> 0xC;
-  this.x = raw >> 0x8 & 0xF;
-  this.y = raw >> 0x4 & 0xF;
-  this.n = raw & 0xF;
-  this.nn = raw & 0xFF;
-  this.nnn = raw & 0xFFF;
 }
+
+/**
+* Read-only property to get the most significant 4 bits of an instruction (the Opcode).
+**/
+Object.defineProperty(Instruction.prototype, 'opcode', {
+  get: function() { return this.raw >> 0xC; }
+});
+
+/**
+* Read-only property to get the second most significant 4 bits of an instruction (the `X` value).
+**/
+Object.defineProperty(Instruction.prototype, 'x', {
+  get: function() { return this.raw >> 0x8 & 0xF; }
+});
+
+/**
+* Read-only property to get the third most significant 4 bits of an instruction (the `Y` value).
+**/
+Object.defineProperty(Instruction.prototype, 'y', {
+  get: function() { return this.raw >> 0x4 & 0xF; }
+});
+
+/**
+* Read-only property to get the least significant 4 bits of an instruction (the `N` value).
+**/
+Object.defineProperty(Instruction.prototype, 'n', {
+  get: function() { return this.raw & 0xF; }
+});
+
+/**
+* Read-only property to get the least significant 8 bits of an instruction (the `NN` value).
+**/
+Object.defineProperty(Instruction.prototype, 'nn', {
+  get: function() { return this.raw & 0xFF; }
+});
+
+/**
+* Read-only property to get the least significant 12 bits of an instruction (the `NNN` value).
+**/
+Object.defineProperty(Instruction.prototype, 'nnn', {
+  get: function() { return this.raw & 0xFFF; }
+});
 
 /**
 * Decode and return a new Instruction object.
