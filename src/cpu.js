ed6d647da0dd8432eefee797853707286b30c07f
R. Tyler Julian <trjulian@gmail.com>
2016-01-23 18:03:06 -0800

    cpu: document method functionality

--- src/cpu.js
+++ src/cpu.js
@@ -20,13 +20,23 @@ function CPU(mm, display, input, loader, clock_frequency) {
 CPU.DEFAULT_CLOCK_FREQUENCY = 1000; // Cycles per second
 
 /**
-* Initialize the CPU, set it's memory, read in the ROM, and start the cycle
+* Initialize the CPU, set it's memory, read in the ROM, and start the cycle.
+* @arg {String} rom - the name of the ROM to load into memory.
 **/
 CPU.prototype.initialize = function(rom) {
+  // Set the current ROM name
   this.current_rom = rom;
+
+  // Initialize the CPU's memory
   this.mm.initialize();
+
+  // Retrieve the ROM from its location
   this.loader.read_rom(this.current_rom, function(rom) {
+
+    // Load the ROM into memory after successful retrieval
     this.mm.load_into_memory(rom);
+
+    // Start the CPU cycle, and decode a number of instructions per cycle equal to the clock frequency
     setInterval(function() {
       for(var i = 0; i < Math.floor(this.clock.frequency / CPU.DEFAULT_CLOCK_FREQUENCY); i++) {
         this.cycle();
@@ -36,11 +46,16 @@ CPU.prototype.initialize = function(rom) {
 };
 
 /**
-* Run a cycle of the CPU's fetch-decode-execute loop.
+* Run a single cycle of the CPU's fetch-decode-execute loop.
 **/
 CPU.prototype.cycle = function() {
+  // Verify that we are not "blocking" on some input
   if (!this.input.is_awaiting) {
+
+    // Increment the internal clock
     this.clock.tick();
+
+    // Fetch-Decode-Execute: Execute(Decode(Fetch()))
     this.executor.execute(this.decoder.decode(this.mm.read()));
   }
 };
